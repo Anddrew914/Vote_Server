7edb7d8367bb260e1de78d260ad0f12144460715
Andrew.Easterling <Anddrew914@gmail.com>
2017-06-25 18:23:12 -0400

    Extends next to declare a winner when entries.size === 1
    'next' now has an if statement looking for the entries.size.
    When this === 1 it removes the vote node, entries node, and sets the
    winner node to the only entry in 'entries'.
    It removes the other nodes instead of just returning a new map with
    only the winner node to avoid potential issues in the future with
    destroying unrelated data. (Not returning it)

--- src/core.js
+++ src/core.js
@@ -1,4 +1,7 @@
-import {List, Map} from 'immutable';
+import {
+  List,
+  Map
+} from 'immutable';
 
 export function setEntries(state, entries) {
   return state.set('entries', List(entries)); //List makes entries immutable to pass testing
@@ -9,18 +12,26 @@ function getWinners(vote) {
   const [a, b] = vote.get('pair');
   const aVotes = vote.getIn(['tally', a], 0);
   const bVotes = vote.getIn(['tally', b], 0);
-  if      (aVotes > bVotes)  return [a];
-  else if (aVotes < bVotes)  return [b];
-  else                       return [a, b];
+  if (aVotes > bVotes) return [a];
+  else if (aVotes < bVotes) return [b];
+  else return [a, b];
 }
 
 export function next(state) {
   const entries = state.get('entries')
-                       .concat(getWinners(state.get('vote')));
-  return state.merge({
-    vote: Map({pair: entries.take(2)}), //take make a new map with just the first (n)
-    entries: entries.skip(2) //skip makes a map without the first(n)
-  });
+    .concat(getWinners(state.get('vote')));
+  if (entries.size === 1) {
+    return state.remove('vote')
+      .remove('entries')
+      .set('winner', entries.first());
+  } else {
+    return state.merge({
+      vote: Map({
+        pair: entries.take(2)
+      }), //take make a new map with just the first (n)
+      entries: entries.skip(2) //skip makes a map without the first(n)
+    });
+  }
 }
 
 export function vote(state, entry) {
