c97d83cb5a312b47df73e5990d881ce415df31f1
Andrew.Easterling <Anddrew914@gmail.com>
2017-06-24 22:14:28 -0400

    Adds 'next' function.
    'Next' gets called on the current state with no other params, it splits
    the currentState 'entries' key into 'entries' and 'vote' keys using
    state.merge with the .take and .skip Map methods (facebook immutable.js)

--- src/core.js
+++ src/core.js
@@ -7,7 +7,7 @@ export function setEntries(state, entries) {
 export function next(state) {
   let entries = state.get('entries');
   return state.merge({
-    vote: Map({pair: entries.take(2)}),
-    entries: entries.skip(2)
+    vote: Map({pair: entries.take(2)}), //take make a new map with just the first (n)
+    entries: entries.skip(2) //skip makes a map without the first(n)
   });
 }
